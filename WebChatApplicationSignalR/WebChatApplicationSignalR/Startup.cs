89935f16596df5d21a08e1ecb03e41c4229060a7
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-09-07 11:27:15 +0200

    libraries, startup classes

---
+++ WebChatApplicationSignalR/WebChatApplicationSignalR/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebChatApplicationSignalR.Startup))]
+
+namespace WebChatApplicationSignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}

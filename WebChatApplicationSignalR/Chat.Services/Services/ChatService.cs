a86ed15fe64b3918c0127e71c501e11759850559
Ruijs,Tom T.J <tom.ruijs@student.fontys.nl>
2017-09-13 14:20:14 +0200

    Inbetween commit

---
+++ WebChatApplicationSignalR/Chat.Services/Services/ChatService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Chat.Data;
+
+namespace Chat.Services.Services {
+    public class ChatService : IChatService {
+        private ChatDbEntities db = new ChatDbEntities();
+
+        public IEnumerable<chat> GetAllChats()
+        {
+            return db.chats;
+        }
+
+        public IEnumerable<chat> GetChatsByChatterId(int chatterId)
+        {
+            var chats = GetAllChats();
+            var queryResult =
+                from chat in chats
+                where chat.ChatSupporterId == chatterId || chat.ChatClientId == chatterId
+                select chat;
+            return queryResult;
+        }
+
+        public IEnumerable<message> GetAllMessages()
+        {
+            return db.messages;
+        }
+
+        public IEnumerable<message> GetMessagesByChatId(int chatId)
+        {
+            var messages = GetAllMessages();
+            var queryResult =
+                from message in messages
+                where message.ChatId == chatId
+                select message;
+            return queryResult;
+        }
+    }
+}
---
+++ WebChatApplicationSignalR/Chat.Services/Services/IChatService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Chat.Data;
+
+namespace Chat.Services.Services {
+    public interface IChatService {
+        IEnumerable<chat> GetAllChats();
+        IEnumerable<chat> GetChatsByChatterId(int chatterId);
+
+        IEnumerable<message> GetAllMessages();
+        IEnumerable<message> GetMessagesByChatId(int chatId);
+
+        void CreateChat(int chatClientId, int chatSupporterId);
+        void CloseChat(int chatId);
+        void SendMessage(string content, int chatId, int chatClientId, int chatSupporterId);
+
+
+    }
+}

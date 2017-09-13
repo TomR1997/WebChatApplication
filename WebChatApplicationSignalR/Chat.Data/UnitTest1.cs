c59b0412b2d7df1b34ecaf64c2d7e001dd97e10c
Ruijs,Tom T.J <tom.ruijs@student.fontys.nl>
2017-09-13 13:04:16 +0200

    Better gitignore and removed generated code

---
+++ WebChatApplicationSignalR/Chat.Data/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chat.Data {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ WebChatApplicationSignalR/WebChatApplicationSignalRTest/GeneratedWebAPITests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebChatApplicationSignalR;
+using WebChatApplicationSignalR.Controllers;
+
+namespace WebChatApplicationSignalRTest {
+    [TestClass]
+    public class APITests {
+
+        [TestMethod]
+        public void GetAllChatters()
+        {
+
+        }
+    }
+}

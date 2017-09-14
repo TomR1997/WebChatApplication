4f1bc1d8cc798e4fe99c6455e380eb662ce2c207
Ruijs,Tom T.J <tom.ruijs@student.fontys.nl>
2017-09-14 13:54:48 +0200

    Git ignore

--- WebChatApplicationSignalR/Chat.Domain/Models/Status.cs
+++ WebChatApplicationSignalR/Chat.Domain/Models/Status.cs
@@ -1,1 +1,10 @@
-﻿public enum Status { Active, Closed }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebChat.Domain.Models {
+    public enum Status {
+        Active, Closed
+    }
+}

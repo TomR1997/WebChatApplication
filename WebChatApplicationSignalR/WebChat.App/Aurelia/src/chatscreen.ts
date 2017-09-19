d592dfc4bc3cce0cb8ba9e918747f2fe844f51e2
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-09-19 14:30:08 +0200

    added js method

--- WebChatApplicationSignalR/WebChat.App/Aurelia/src/chatscreen.ts
+++ WebChatApplicationSignalR/WebChat.App/Aurelia/src/chatscreen.ts
@@ -1,10 +1,52 @@
-﻿export class ChatScreen {
-    private connection: SignalR;
+﻿interface SignalR {
+    groupChatHub: {
+        client: {
+            publishMessage(name: string, message: string)
+        };
 
-    constructor() {
+        server: {
+            sendMessage(name: string, message: string)
+        };
     }
+}
 
+export class ChatScreen {
+    private connection: SignalR;
     sendMessage() {
-        alert('test');
+        var $: any;
+        $(function () {
+        
+            var ticker = $.connection.groupChatHub;
+            $.connection.hub.start();
+
+            $.extend(ticker.client, {
+
+                publishMessage: function (data) {
+                    $("#msg").append("<li><span class='p'>" + data.Name + "：</span>" + data.Msg + " <span class='time'>" + data.Time + "</span></li>")
+                    $("#msg").parents("div")[0].scrollTop = $("#msg").parents("div")[0].scrollHeight;
+                },
+
+                publishUser: function (data) {
+                    $("#count").text(data.length);
+                    $("#users").empty();
+                    $("#users").append('<option value="0">Everyone</option>');
+                    for (var i = 0; i < data.length; i++) {
+                        $("#users").append('<option value="' + data[i] + '">' + data[i] + '</option>')
+                    }
+
+                }
+            });
+
+            $("#btn-send").click(function () {
+                var msg = $("#txt-msg").val();
+                if (!msg) {
+                    alert('Error...'); return false;
+                }
+                $("#txt-msg").val('');
+
+                ticker.server.sendMessage($("#users").val(), msg);
+            });
+
+        });
     }
 }

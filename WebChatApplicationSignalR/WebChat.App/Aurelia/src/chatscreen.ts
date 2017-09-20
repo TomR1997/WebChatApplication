59d8695dd4f1ca111b3b9bb6836cf88ffb97fb58
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-09-20 09:19:08 +0200

    xunit

--- WebChatApplicationSignalR/WebChat.App/Aurelia/src/chatscreen.ts
+++ WebChatApplicationSignalR/WebChat.App/Aurelia/src/chatscreen.ts
@@ -22,29 +22,29 @@ export class ChatScreen {
             $.extend(ticker.client, {
 
                 publishMessage: function (data) {
-                    $("#msg").append("<li><span class='p'>" + data.Name + "：</span>" + data.Msg + " <span class='time'>" + data.Time + "</span></li>")
-                    $("#msg").parents("div")[0].scrollTop = $("#msg").parents("div")[0].scrollHeight;
+                    $('#msg').append('<li><span class="p">' + data.Name + '：</span>' + data.Msg + ' <span class="time">' + data.Time + '</span></li>')
+                    $('#msg').parents('div')[0].scrollTop = $('#msg').parents('div')[0].scrollHeight;
                 },
 
                 publishUser: function (data) {
-                    $("#count").text(data.length);
-                    $("#users").empty();
-                    $("#users").append('<option value="0">Everyone</option>');
+                    $('#count').text(data.length);
+                    $('#users').empty();
+                    $('#users').append('<option value="0">Everyone</option>');
                     for (var i = 0; i < data.length; i++) {
-                        $("#users").append('<option value="' + data[i] + '">' + data[i] + '</option>')
+                        $('#users').append('<option value=' + data[i] + '>' + data[i] + '</option>')
                     }
 
                 }
             });
 
-            $("#btn-send").click(function () {
-                var msg = $("#txt-msg").val();
+            $('#btn-send').click(function () {
+                var msg = $('#txt-msg').val();
                 if (!msg) {
                     alert('Error...'); return false;
                 }
-                $("#txt-msg").val('');
+                $('#txt-msg').val('');
 
-                ticker.server.sendMessage($("#users").val(), msg);
+                ticker.server.sendMessage($('#users').val(), msg);
             });
 
         });

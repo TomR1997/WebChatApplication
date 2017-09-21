51b87c0854f5d9532a7978d553abf95223c82343
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-09-21 10:37:38 +0200

    test

--- WebChatApplicationSignalR/UnitTest/AccountControllerTest.cs
+++ WebChatApplicationSignalR/UnitTest/AccountControllerTest.cs
@@ -2,13 +2,16 @@
 using Xunit;
 using System.Web.Http;
 using System.Web.Http.Results;
+using System.Web;
+using System.Web.Mvc;
+using System;
 
 namespace UnitTest
 {
     public class AccountControllerTest
     {
-        private AccountController accountController;
-
+        private readonly AccountController accountController;
+       
         public AccountControllerTest()
         {
             accountController = new AccountController();
@@ -16,9 +19,46 @@ namespace UnitTest
         [Fact]
         public void LogOutTest()
         {
+            /* test cookies
+            var httpContext = new MockHttpContext();
+   
             IHttpActionResult actionResult = accountController.Logout();
 
-            Assert.IsType(typeof(OkResult), actionResult);
+            Assert.IsType(typeof(OkResult), actionResult);*/
+            throw new NotSupportedException();
+        }
+    }
+
+    public class MockHttpContext : HttpContextBase
+    {
+        readonly HttpRequestBase _request;
+
+        public MockHttpContext()
+        {
+            _request = new MockHttpRequest();
+        }
+
+        public override HttpRequestBase Request
+        {
+            get { return _request; }
+        }
+
+        class MockHttpRequest : HttpRequestBase
+        {
+            readonly HttpCookieCollection _cookies;
+
+            public MockHttpRequest()
+            {
+                _cookies = new HttpCookieCollection();
+            }
+
+            public override HttpCookieCollection Cookies
+            {
+                get
+                {
+                    return _cookies;
+                }
+            }
         }
     }
 }

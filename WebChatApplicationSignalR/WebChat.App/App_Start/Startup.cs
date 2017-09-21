c659a65ae5b6b3a991d6d65da184249842d6c17f
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-09-21 13:24:40 +0200

    packages startup class

---
+++ WebChatApplicationSignalR/WebChat.App/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebChat.App.App_Start.Startup))]
+
+namespace WebChat.App.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+           
+        }
+    }
+}

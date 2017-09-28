2481651da0bd0c952652b0f3f2916c13e97a3967
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-09-28 14:48:36 +0200

    removed useless stuf

--- WebChatApplicationSignalR/WebChat.App/Aurelia/src/chatscreen.ts
+++ WebChatApplicationSignalR/WebChat.App/Aurelia/src/chatscreen.ts
@@ -28,17 +28,11 @@ export class ChatScreen {
         }
         hubName = hubName.toLocaleLowerCase();
         if (!this.connection.proxies[hubName]) {
-            this.hubProxy = this.connection.createHubProxy(hubName);
-            /*hubProxy.on('sendMessage', function (data) {
-                console.log(data);
-            });*/  
+            this.hubProxy = this.connection.createHubProxy(hubName); 
             this.connection.proxies[hubName].funcs = {};
-            /*this.connection.proxies[hubName].on('sendMessage', function(data){
-                console.log(data + ' data');
-            });*/
 
             //this.hubProxy.on('sendMessage', message => this.onMessageReceived(message));
-            this.hubProxy.on('addNewMessageToPage', message => console.log(message));
+            this.hubProxy.on('addNewMessageToPage', message => this.onMessageReceived(message));
         }
     }
 
@@ -98,56 +92,8 @@ export class ChatScreen {
         $("#msg").append("<li><span class='p'>" +'someName' + "：</span>" + this.message + " <span class='time'>" + 'date' + "</span></li>")
         $("#msg").parents("div")[0].scrollTop = $("#msg").parents("div")[0].scrollHeight;
         hub.invoke('Send', 'TestName', this.message);
-        /*
-        $.extend(hub.client, {
-
-
-            publshMsg: function (data) {
-                $("#msg").append("<li><span class='p'>" + data.Name + "：</span>" + data.Msg + " <span class='time'>" + data.Time + "</span></li>")
-                $("#msg").parents("div")[0].scrollTop = $("#msg").parents("div")[0].scrollHeight;
-                console.log(data.Name + ' ' + data.Msg);
-            },
-
-            publshUser: function (data) {
-                $("#count").text(data.length);
-                $("#users").empty();
-                $("#users").append('<option value="0">Everyone</option>');
-                for (var i = 0; i < data.length; i++) {
-                    $("#users").append('<option value="' + data[i] + '">' + data[i] + '</option>')
-                }
-
-            }
-        });*/
-
-        /*$("#btn-send").click(function () {
-            var msg = $("#txt-msg").val();
-            if (!msg) {
-                alert('Error...'); return false;
-            }
-            $("#txt-msg").val('');
-
-            //hub.server.sendMessage($("#users").val(), msg);
-            hub.invoke('SendMessage', { 'name': String, msg: String }).done(function () {
-                console.log('new message invocation');
-            }).fail(function (err) {
-                console.log('new message failed ' + err);
-                });
-        });*/
-        /*
-        hub.invoke('Send', 'TestName', this.message);
-
-        /*hub.client.addNewMessageToPage = function (name, message) {
-            $("#id").append('<ul style="list-style-type:square"><li><strong style="color:red;font-style:normal;font-size:medium;text-transform:uppercase">' + 'NameTest' + '  ' + '<strong style="color:black;font-style:normal;font-size:medium;text-transform:lowercase">said</strong>'
-                + '</strong>: ' + '<strong style="color:blue;font-style:oblique;font-size:medium">' + message + '</strong>' + '</li></ul>');
-        }*/
-
-
-        //hub.server.send('nameTest', msg);
-        //$("#txt-msg").val();
-
-
+       
         //hub.invoke('SendMessage', 'name', msg);
-        //this.hubProxy.invoke('invokeMessage', msg);
     }
 
     onMessageReceived(latestMessage: string) {
